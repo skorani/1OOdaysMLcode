51d1a4d8c191d4070b576f24d43a53a70b0b2159
skorani <samira.korani@gmail.com>
2019-11-27 17:58:07 +0330

    Add files via upload

---
+++ get_input.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 10:37:25 2019
+
+@author: Thinkpad
+"""
+
+
+def get_opponent_data():
+    # set the name for players x,y position 
+    opx1 opx2 opx3 opx4 opx5
+    
+    return
+
+
+
+
+dataset = []
+with open ('', 'r') as data:
+    for i in data:
+        data1= data.readlines()
+        data2= primary_frame ( data1,win_size)
+        dataset.append(data2)
+input_data= pandas.DataFrame (data2)
+
+
+def measure_similarity():
+    
---
+++ wholeProcess.py
@@ -0,0 +1,26 @@
+import MicroEvent
+import pandas as pd
+
+time_windows = []
+with open ('sam1.csv','r')as data:
+    Basket_Data= pd.read_csv(data)
+
+Basket_Data_Event =MicroEvent.event()
+Basket_Data_MicroEvent = Basket_Data_Event.get_event()
+
+def Fat_matrix(player,position,flow,ballstate,ball_flow,ball_position):
+    for start_time in MicroEvent:
+        if start_time +1 =
+        return longvector
+    else 
+def ReverseMicroEvent():
+    return 
+
+
+
+
+
+
+
+
+
