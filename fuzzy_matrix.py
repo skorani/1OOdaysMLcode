12ec76a9ff11069b73d0a310f72fbb7be366f1a7
skorani <samira.korani@gmail.com>
2019-11-27 17:53:13 +0330

    Add files via upload

---
+++ fuzzy_matrix.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+
+import pandas
+from sklearn import * 
+import numpy as np
+
+
+
+Region_dictionary= {  }
+
+def region_dic():
+    for key , value in {
+        'A' : 23.5 , 41
+        'B' :
+        'C':
+        'D' :
+        'E':
+        'F':
+        'H':
+        }
+        
+
+def df2matrix(df):
+    
+    # firsst have to drop the gametime, milisecond column.
+    # pandas to numpy.
+    matrix =
+    return matrix
+
+def extract_point_loop(df):
+    
+    a = df.axes
+    fuzzy_matrix= 
+    for 
+        for 
+    
+            df [2] , df[3] = ballx , bally
+            fuzzying_datapoint (x , y , region_dic)
+            
+            for i in a and i > 4 :
+                
+                df[i] , df[i+5] = x , y                 
+                fuzzy_position= fuzzying_datapoint (x , y , region_dic)
+                # append to fuzzy matrix.
+                
+                
+def fuzzying_datapoint (x , y , region_dic):
+    
+    for 
+    
+
+def primary_frame() :
+    
+def create_fuzzy_position():
+    
+def create_fuzzy_flow():
+
+    
+def merge_flow_position(a , b):
+    return pd.concat(a, b , axis=1)
+
+def creaste_fuzzy_ball():
+    
+def create_fuzzy_player():
+
+def create_fuzzy_offender():
+
+def create_fuzzy_defender():
+    
+    
+
+a = create_fuzzy_position()
+b = create_fuzzy_flow()
+
+    
